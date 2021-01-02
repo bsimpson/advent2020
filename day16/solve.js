b1b72f0a5d447fa112078ddb0f5ad1e95bfd47c7
Ben Simpson <thehoagie@gmail.com>
2021-01-02 12:25:33 -0500

    Solved day 16 part two

--- day16/solve.js
+++ day16/solve.js
@@ -9,7 +9,7 @@ try {
 
 function partOne(input = data) {
   let invalidValues = [];
-  let values = nearbyTickets(input);
+  let values = [].concat(...nearbyTickets(input));
   for(let value of values) {
     let match = false;
 
@@ -30,6 +30,121 @@ function partOne(input = data) {
   return invalidValues.reduce((agg, x) => {
     return agg + x;
   }, 0);
+}
+
+function filteredTickets(input = data) {
+  let tickets = nearbyTickets(input);
+  let ranges = rangesObject(input);
+
+  return tickets.filter(ticket => {
+    return ticket.every(value => {
+      let match = false;
+      for(let rule in ranges) {
+        ranges[rule].forEach(([min, max]) => {
+          if (value >= min && value <= max) {
+            match = true;
+          }
+        });
+      }
+      return match;
+    });
+  });
+}
+
+function determineRangePositions(input = data) {
+  let ranges = rangesObject(input);
+  let tickets = filteredTickets(input);
+  let rangePositions = {};
+
+  // for each rule
+  for (let rule in ranges) {
+
+    // for each ticket position
+    for(let x=0; x < tickets[0].length; x++) {
+
+      // do all values at this ticket position match a rule?
+      let result = tickets.every(ticket => {
+        let value = ticket[x];
+        let match = false;
+  
+        ranges[rule].forEach(([min, max]) => {
+          if (value >= min && value <= max) {
+            match = true;
+          }
+        });
+
+        return match;
+      });
+
+      // if they all match, mark that rule with the possible ticket position
+      if (result) {
+        rangePositions[rule] = rangePositions[rule] || [];
+        rangePositions[rule].push(x);
+        // break;
+      }
+    }
+  }
+
+  return unambiguate(rangePositions);
+}
+
+function unambiguate(rangePositions) {
+  if (Object.values(rangePositions).every(positions => positions.length === 1)) {
+    // takes a value like { "class": [1] } and converts to { "class": 1 }
+    for(let rule in rangePositions) {
+      rangePositions[rule] = rangePositions[rule][0];
+    }
+
+    return rangePositions;
+  }
+
+  let definitiveRule;
+  while (!definitiveRule) {
+    for (let rule in rangePositions) {
+      // find a rule with only one position
+      // in which at least one other rule contains the same value
+      if (rangePositions[rule].length === 1 &&
+      [].concat(...Object.values(rangePositions)).filter(x => x === rangePositions[rule][0]).length > 1) {
+        definitiveRule = rule;
+      }
+    }
+  }
+  let definitePosition = rangePositions[definitiveRule][0];
+
+  for (let rule in rangePositions) {
+    if (rule === definitiveRule) {
+      continue;
+    }
+    rangePositions[rule] = rangePositions[rule].filter(position => position !== definitePosition);
+  }
+
+  try {
+    return unambiguate(rangePositions);
+  } catch(err) {
+    console.error(err)
+  }
+}
+// guessed  453459307723 - correct
+// guessed 1382560228363 - too high
+function partTwo(input = data, ruleMatch = /^departure.*/) {
+  let rangePositions = determineRangePositions(input);
+  let ticket = myTicket(input);
+  let product = 1;
+
+  for(let rule in rangePositions) {
+    if (rule.match(ruleMatch)) {
+      // console.log(rule)
+      // console.log(ticket[rangePositions[rule]])
+      product *= ticket[rangePositions[rule]];
+    }
+  }
+
+  return product;
+}
+
+function myTicket(input = data) {
+  let sections = input.split('\n\n');
+  return sections[1].split('\n')[1].split(',').map(x => parseInt(x));
 }
 
 function rangesObject(input = data) {
@@ -56,7 +171,7 @@ function nearbyTickets(input = data) {
     if (seat === 'nearby tickets:')
       continue;
 
-    allSeats.push(...seat.split(',').map(v => parseInt(v)));
+    allSeats.push(seat.split(',').map(v => parseInt(v)));
   }
 
   return allSeats;
@@ -66,4 +181,9 @@ module.exports = {
   rangesObject,
   nearbyTickets,
   partOne,
+  filteredTickets,
+  determineRangePositions,
+  myTicket,
+  partTwo,
+  unambiguate,
 }
--- day16/test/solve.js
+++ day16/test/solve.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const subject = require('../solve.js');
 
-const data = `class: 1-3 or 5-7
+const partOneData = `class: 1-3 or 5-7
 row: 6-11 or 33-44
 seat: 13-40 or 45-50
 
@@ -14,10 +14,22 @@ nearby tickets:
 55,2,20
 38,6,12`;
 
+const partTwoData = `class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9`;
+
 describe('solve', function() {
   describe('rangesObject', function() {
     it('has ranges', function() {
-      const results = subject.rangesObject(data);
+      const results = subject.rangesObject(partOneData);
       assert.deepStrictEqual(results.class, [[1,3], [5,7]]);
       assert.deepStrictEqual(results.row, [[6,11], [33,44]]);
       assert.deepStrictEqual(results.seat, [[13,40], [45,50]]);
@@ -26,13 +38,78 @@ describe('solve', function() {
 
   describe('nearbyTickets', function() {
     it('has array of values', function() {
-      assert.deepStrictEqual(subject.nearbyTickets(data), [7,3,47,40,4,50,55,2,20,38,6,12]);
+      assert.deepStrictEqual(subject.nearbyTickets(partOneData), [
+        [7,3,47],
+        [40,4,50],
+        [55,2,20],
+        [38,6,12],
+      ]);
     });
   });
 
   describe('partOne', function() {
     it('returns sum of invalid values', function() {
-      assert.strictEqual(subject.partOne(data), 71);
+      assert.strictEqual(subject.partOne(partOneData), 71);
     });
   });
+
+  describe('filteredTickets', function() {
+    it('returns product of destination fields', function() {
+      assert.deepStrictEqual(subject.filteredTickets(partOneData), [
+        [7,3,47]
+      ]);
+    });
+  });
+
+  describe('determineRangePositions', function() {
+    it('returns ticket position for each rule', function() {
+      assert.deepStrictEqual(subject.determineRangePositions(partTwoData), {
+        row: 0,
+        class: 1,
+        seat: 2,
+      });
+    });
+  });
+
+  describe('myTicket', function() {
+    it('returns my ticket values', function() {
+      assert.deepStrictEqual(subject.myTicket(partTwoData), [
+        11,12,13
+      ]);
+    });
+  });
+
+  describe('partTwo', function() {
+    it('returns the product of departure fields', function() {
+      assert.strictEqual(subject.partTwo(partTwoData, /^class.*/), 12);
+      assert.strictEqual(subject.partTwo(partTwoData, /^row.*/), 11);
+      assert.strictEqual(subject.partTwo(partTwoData, /s/), 156);
+      assert.strictEqual(subject.partTwo(partTwoData, /.*/), 1716);
+    });
+  });
+
+  describe('unambiguate', function() {
+    it('returns correct positions', function() {
+      let rangePositions = {
+        "class": [
+          1,
+          2,
+        ],
+        "row": [
+          0,
+          1,
+          2,
+        ],
+        "seat": [
+          2,
+        ],
+      }
+
+      assert.deepStrictEqual(subject.unambiguate(rangePositions), {
+        row: 0,
+        class: 1,
+        seat: 2,
+      });
+    });
+  })
 });
